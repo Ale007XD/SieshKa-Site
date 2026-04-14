0ef3cf78fcfa468df8f51d01d0f29f15427055b1
Ale007XD <alex.deloverov@gmail.com>
2026-04-14 16:36:50 +0800

    Update payments.py

--- app/payments.py
+++ app/payments.py
@@ -113,6 +113,7 @@ def create_yookassa_payment(order: Order, db: Session) -> str:
                 "currency": "RUB",
             },
             "capture": True,
+            "payment_method_types": ["bank_card", "sbp"],
             "confirmation": {
                 "type": "redirect",
                 "return_url": _build_return_url(order),
